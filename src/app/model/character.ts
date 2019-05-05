2b15f87c743b435664cca0cb447c00d6fcab459f
Courtney Villhauer <courtney.villhauer@gmail.com>
2019-05-05 12:50:06 -0500

    Switching spellGroup calculation to a switch statement

--- src/app/model/character.ts
+++ src/app/model/character.ts
@@ -18,11 +18,23 @@ export class Character {
 
     calculateSpellGroup() {
         let spellGroup = "";
-        if (this.characterClass === "Druid") {
-            spellGroup = "summonnaturesally"
-        }
-        else if (this.characterClass) {
-            spellGroup = "summonmonster"
+        switch(this.characterClass){
+            case "Druid":
+            case "Ranger":
+            case "Shaman":
+                spellGroup = "summonnaturesally";
+                break;
+            case "Bard":
+            case "Cleric":
+            case "Oracle":
+            case "Sorcerer":
+            case "Summoner":
+            case "Witch":
+            case "Wizard":
+                spellGroup = "summonmonster";
+                break;
+            default:
+                break;
         }
         return spellGroup;
     }
