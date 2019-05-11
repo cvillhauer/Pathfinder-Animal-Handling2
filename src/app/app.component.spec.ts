3bb45e9c5ce113dffeac336a260f5fc33afa51a4
R. Richards <rdrrichards@hotmail.com>
2019-05-11 14:37:53 -0500

    app comp unit test working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './components/character/character.component';
+import { FormsModule } from '@angular/forms';
+import { SpellComponent } from './components/spell/spell.component';
+import { CreatureComponent } from './components/creature/creature.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, FormsModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, CharacterComponent, SpellComponent, CreatureComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +23,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'Pathfinder-Animal-Handling2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Pathfinder-Animal-Handling2');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Pathfinder-Animal-Handling2!');
   });
 });
