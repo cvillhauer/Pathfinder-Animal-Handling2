9ae5308826681b2e2dc12b71627d7389b4d3cf19
R. Richards <rdrrichards@hotmail.com>
2019-05-11 14:59:55 -0500

    createure spec; creature refactor-ish

---
+++ src/app/components/creature/creature.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { CreatureComponent } from './creature.component';
+import { FormsModule } from '@angular/forms';
+
+describe('CreatureComponent', () => {
+  let component: CreatureComponent;
+  let fixture: ComponentFixture<CreatureComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreatureComponent ],
+      imports: [ RouterTestingModule, HttpClientTestingModule, FormsModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreatureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
