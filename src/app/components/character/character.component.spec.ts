95cbc750516af7c7ca879d211c39014e09172311
R. Richards <rdrrichards@hotmail.com>
2019-05-11 14:56:33 -0500

    character spec; small character refactor

---
+++ src/app/components/character/character.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { CharacterComponent } from './character.component';
+import { FormsModule } from '@angular/forms';
+import { SpellComponent } from '../spell/spell.component';
+import { CreatureComponent } from '../creature/creature.component';
+
+describe('CharacterComponent', () => {
+  let component: CharacterComponent;
+  let fixture: ComponentFixture<CharacterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CharacterComponent, SpellComponent, CreatureComponent ],
+      imports: [ RouterTestingModule, HttpClientTestingModule, FormsModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CharacterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
