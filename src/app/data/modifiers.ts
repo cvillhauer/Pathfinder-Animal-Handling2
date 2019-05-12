cba5b94f332770f49aba697be550ba8ba88bb029
R. Richards <rdrrichards@hotmail.com>
2019-05-12 11:19:00 -0500

    data refactored to their own files

---
+++ src/app/data/modifiers.ts
@@ -0,0 +1,26 @@
+export const modifiers = [
+  {
+    id: 'str',
+    description: 'Strength',
+  },
+  {
+    id: 'dex',
+    description: 'Dexterity',
+  },
+  {
+    id: 'con',
+    description: 'Constitution',
+  },
+  {
+    id: 'int',
+    description: 'Intelligence',
+  },
+  {
+    id: 'wis',
+    description: 'Wisdom',
+  },
+  {
+    id: 'cha',
+    description: 'Charisma',
+  },
+];
