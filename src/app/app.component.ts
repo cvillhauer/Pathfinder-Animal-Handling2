e670dd42bab0cc3db46e5b538cea69cd89e4ec5c
Courtney Villhauer <courtney.villhauer@gmail.com>
2019-05-13 20:24:37 -0500

    Added AbilityScore to character

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from './model/character';
+import { AbilityScores } from './model/abilityscores';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,9 @@ export class AppComponent implements OnInit {
   characters: Character[] = [];
 
   ngOnInit() {
-    let courtney: Character = new Character("courtney", "Courtney", "Druid", 6);
-    courtney.feats.push("Augmented Summoning");
-    let richard: Character = new Character("richard", "Richard", "Wizard", 1);
+    const courtney: Character = new Character('courtney', 'Courtney', 'Druid', 6, new AbilityScores(12, 16, 13, 12, 16, 8));
+    courtney.feats.push('Augmented Summoning');
+    const richard: Character = new Character('richard', 'Richard', 'Wizard', 1, new AbilityScores(10, 14, 13, 18, 12, 10));
     this.characters.push(courtney);
     this.characters.push(richard);
   }
--- src/app/model/character.ts
+++ src/app/model/character.ts
@@ -1,5 +1,6 @@
 import { Spell } from './spell';
 import { Creature } from './creature';
+import { AbilityScores } from './abilityscores';
 
 export class Character {
     id: string;
@@ -11,14 +12,17 @@ export class Character {
     spellGroup: string;
     spellLevel: number;
     feats: string[];
+    abilityScores: AbilityScores;
+
     summonedCreatures: Creature[];
 
-    constructor(id: string, name: string, characterClass: string, level: number) {
+    constructor(id: string, name: string, characterClass: string, level: number, scores: AbilityScores) {
         this.id = id;
         this.characterName = name;
         this.characterClass = characterClass;
         this.characterLevel = level;
         this.feats = [];
+        this.abilityScores = scores;
         this.summonedCreatures = [];
     }
 
